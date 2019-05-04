37b26a2d14900b487d57915b4726d890c2426170
Erick Guan <fantasticfears@gmail.com>
2019-05-04 23:44:17 +0200

    update more models

--- processdata.py
+++ processdata.py
@@ -11,6 +11,11 @@ def tokenize(l):
       if c != '':
         yield(c)
 
+def tokenize_num(l):
+  for b in l.rstrip().split(' '):
+    if b != '':
+      yield(b)
+
 def get_vocabs(*files):
   vocab_set = set()
 
@@ -24,7 +29,32 @@ def get_vocabs(*files):
     
   return list(vocab_set | set([START_SYMBOL, STOP_SYMBOL]))
 
-def generate_mapping_from_vocabs(vocab):
-  with open('data/loglinear/vocab_mapping.txt', 'w') as f:
+def generate_mapping_from_vocabs(filename, vocab):
+  with open(filename, 'w') as f:
     for i, v in enumerate(vocab):
       f.write(f"{v}\t{i}\n")
+
+def load_mapping_from_vocabs(file, convert_to_int=True):
+  mapping = {}
+  with open(file, 'r') as f:
+    for l in f:
+      k, v = l.rstrip().split('\t')
+      if convert_to_int:
+        mapping[k] = int(v)
+      else:
+        mapping[k] = v
+  return mapping
+
+def transform_data(mapping, file):
+  seqs = []
+  with open(file, 'r') as f:
+    for l in f:
+      line_mapping = [mapping[t] if t in mapping else t for t in tokenize(l) ]
+      seqs.append(line_mapping)
+  
+  return seqs
+
+def writing_seq_idx(filename, seqs):
+  with open(filename, 'w') as f:
+    for l in seqs:
+      f.write(" ".join(l) + "\n")
